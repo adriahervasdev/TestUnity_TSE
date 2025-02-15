1e919433a3b801aa71f3d123cd4b6c4c1970c9de
adriahervasdev <adriahervas.dev@gmail.com>
2025-02-15 13:52:14 +0100

    Scene manager added in the window editor

---
+++ Assets/TestUnity_TSE/Scripts/Editor/SceneManagerEditorWindow.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class SceneManagerEditorWindow : EditorWindow
+{
+    private SceneManagerScriptableObject SceneManagerSO;
+    private SceneAsset DropSceneAsset;
+
+    // Add custom window to Unity editor
+    [MenuItem("Window/Scene Manager")]
+    public static void ShowWindow()
+    {
+        GetWindow<SceneManagerEditorWindow>("Scene Manager Editor Window");
+    }
+
+    // Draw custom UI for the SceneManager
+    private void OnGUI()
+    {
+        // Assign the SceneManagerSO from the Inspector.
+        SceneManagerSO = (SceneManagerScriptableObject)EditorGUILayout.ObjectField("Scene Manager SO", SceneManagerSO, typeof(SceneManagerScriptableObject), false);
+
+        if (SceneManagerSO == null)
+        {
+            EditorGUILayout.HelpBox("Assign a SceneManagerSO asset.", MessageType.Warning);
+            return;
+        }
+
+        // List the scenes currently in Build Settings
+        GUILayout.Label("Scenes currently in Build Settings", EditorStyles.boldLabel);
+        foreach (EditorBuildSettingsScene Scene in EditorBuildSettings.scenes)
+        {
+            if (!Scene.enabled) continue;
+
+            GUILayout.BeginHorizontal();
+            GUILayout.Label(Scene.path);
+            if (GUILayout.Button("Cargar"))
+            {
+                SceneManagerSO.LoadScene(Scene.path);
+            }
+            GUILayout.EndHorizontal();
+        }
+
+        // Add button to reload the current scene
+        GUILayout.Space(25);
+        if (GUILayout.Button("Reload current scene"))
+        {
+            SceneManagerSO.ReloadCurrentScene();
+        }
+
+        // Add field to add scene to Build Settings
+        GUILayout.Space(25);
+        GUILayout.Label("Add scene to Build Settings", EditorStyles.boldLabel);
+        DropSceneAsset = (SceneAsset)EditorGUILayout.ObjectField("Arrastra una escena", DropSceneAsset, typeof(SceneAsset), false);
+        if (DropSceneAsset != null && GUILayout.Button("Add scene to Build Settings"))
+        {
+            string ScenePath = AssetDatabase.GetAssetPath(DropSceneAsset);
+            SceneManagerSO.AddSceneToBuild(ScenePath);
+            DropSceneAsset = null;
+        }
+    }
+}
---
+++ Assets/TestUnity_TSE/Scripts/Editor/SceneManagerScriptableObject.cs
@@ -0,0 +1,60 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SceneManagerScriptableObject", menuName = "Custom/SceneManagerSO")]
+public class SceneManagerScriptableObject : ScriptableObject
+{
+    // Function to load a scene
+    public void LoadScene(string ScenePath)
+    {
+        if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+        {
+            EditorSceneManager.OpenScene(ScenePath);
+            Debug.Log("Load Scene: " + ScenePath);
+        }
+    }
+
+    // Function to reload the current scene
+    public void ReloadCurrentScene()
+    {
+        string CurrentScene = EditorSceneManager.GetActiveScene().path;
+        if (!string.IsNullOrEmpty(CurrentScene))
+        {
+            if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+            {
+                EditorSceneManager.OpenScene(CurrentScene);
+                Debug.Log("Scene reloaded successfully: " + CurrentScene);
+            }
+        }
+        else
+        {
+            EditorUtility.DisplayDialog("Attention", "No active scene found.", "OK");
+            Debug.LogWarning("The scene to reload is invalid or not found: " + CurrentScene);
+        }
+    }
+
+    // Function to add scene to Build Settings
+    public void AddSceneToBuild(string ScenePath)
+    {
+        // Check if the scene has already been added to Build Settings
+        EditorBuildSettingsScene[] CurrentScenes = EditorBuildSettings.scenes;
+        foreach (var s in CurrentScenes)
+        {
+            if (s.path == ScenePath)
+            {
+                EditorUtility.DisplayDialog("Attention", "The scene is already in Build Settings.", "OK");
+                Debug.LogWarning("The scene is already added to the Build Settings: " + ScenePath);
+                return;
+            }
+        }
+
+        // Add scene to Build Settings
+        EditorBuildSettingsScene[] NewScenes = new EditorBuildSettingsScene[CurrentScenes.Length + 1];
+        System.Array.Copy(CurrentScenes, NewScenes, CurrentScenes.Length);
+        NewScenes[NewScenes.Length - 1] = new EditorBuildSettingsScene(ScenePath, true);
+        EditorBuildSettings.scenes = NewScenes;
+        EditorUtility.DisplayDialog("Success", "The scene has been added to Build Settings.", "OK");
+        Debug.Log("The scene has been successfully added to Build Settings: " + ScenePath);
+    }
+}
